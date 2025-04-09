340ca86bfce99a389e192e436f44d8ddd8f8c489
Brian <okellobrian18@gmail.com>
2025-04-09 14:36:05 +0300

    lists and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const displayLinks = links.map(link => {
+    return <a key={`#${link}`} href={`#${link}`} >{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {displayLinks}
+    </nav>
+  )
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const listTechnologies = technologies.map((technology, index) => (
+    <span key={index}>{technology}</span>
+  ))
+
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {listTechnologies}
       </div>
     </div>
   );
